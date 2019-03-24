c4a124ccbdd4632417a4b2c6e5fdf794c4191545
Carl Kavanagh <alterego01010@gmail.com>
2019-03-24 13:53:58 +0000

    Setup

---
+++ html/src/com/aerie/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.aerie.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.aerie.Aerie;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Aerie();
+        }
+}
